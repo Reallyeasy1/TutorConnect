34a720a4c58426eab7889969bbe3ed452a0fbb10
Reallyeasy1 <loyongzhe@gmail.com>
2024-06-28 14:11:09 +0800

    Deployment

--- tutor-connect/app/api/auth/[...nextauth]/authOptions.ts
+++ tutor-connect/app/api/auth/[...nextauth]/authOptions.ts
@@ -93,3 +93,4 @@ export const authOptions: NextAuthOptions = {
     },
   },
 };
+
--- tutor-connect/app/api/auth/[...nextauth]/route.ts
+++ tutor-connect/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 // app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
-import { authOptions } from "./authOptions"; // Ensure the correct path to authOptions
+import { authOptions } from "../authOptions"; // Ensure the correct path to authOptions
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
