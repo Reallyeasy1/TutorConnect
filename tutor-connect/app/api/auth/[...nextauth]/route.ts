26fbab7750e60287173edc0319dbeb79ded77420
Reallyeasy1 <loyongzhe@gmail.com>
2024-06-28 13:42:38 +0800

    Deployment

--- tutor-connect/app/api/auth/[...nextauth]/route.ts
+++ tutor-connect/app/api/auth/[...nextauth]/route.ts
@@ -3,100 +3,98 @@ import { compare } from "bcrypt";
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions: NextAuthOptions = {
-    pages: {
-        signIn: "/login",
-    },
-    session: {
-        strategy: "jwt",
-    },
-    providers: [
-        CredentialsProvider({
-            name: "Sign in",
-            credentials: {
-                email: {
-                    label: "Email",
-                    type: "email",
-                    placeholder: "hello@example.com",
-                },
-                password: { label: "Password", type: "password" },
-                typeOfTutor: { label: "typeOfTutor", type: "typeOfTutor" },
-            },
-            async authorize(credentials) {
-                if (!credentials?.email || !credentials.password) {
-                    return null;
-                }
+export const authOptions = {
+	pages: {
+		signIn: "/login",
+	},
+	session: {
+		strategy: "jwt",
+	},
+	providers: [
+		CredentialsProvider({
+			name: "Sign in",
+			credentials: {
+				email: {
+					label: "Email",
+					type: "email",
+					placeholder: "hello@example.com",
+				},
+				password: { label: "Password", type: "password" },
+				typeOfTutor: { label: "typeOfTutor", type: "typeOfTutor" },
+			},
+			async authorize(credentials) {
+				if (!credentials?.email || !credentials.password) {
+					return null;
+				}
 
-                let user;
+				let user;
 
-                if (credentials?.typeOfTutor) {
-                    user = await prisma.tutor.findUnique({
-                        where: {
-                            email: credentials.email,
-                        },
-                    });
-                } else {
-                    user = await prisma.client.findUnique({
-                        where: {
-                            email: credentials.email,
-                        },
-                    });
-                }
+				if (credentials?.typeOfTutor) {
+					user = await prisma.tutor.findUnique({
+						where: {
+							email: credentials.email,
+						},
+					});
+				} else {
+					user = await prisma.client.findUnique({
+						where: {
+							email: credentials.email,
+						},
+					});
+				}
 
-                if (!user) {
-                    return null;
-                }
+				if (!user) {
+					return null;
+				}
 
-                if (!user.active) {
-                    throw new Error("User is not active");
-                }
+				if (!user.active) {
+					throw new Error("User is not active");
+				}
 
-                const isPasswordValid = await compare(
-                    credentials.password,
-                    user.password
-                );
+				const isPasswordValid = await compare(
+					credentials.password,
+					user.password
+				);
 
-                if (!isPasswordValid) {
-                    return null;
-                }
+				if (!isPasswordValid) {
+					return null;
+				}
 
-                return {
-                    id: user.id + "",
-                    email: user.email,
-                    name: user.name,
-                    randomKey: credentials?.typeOfTutor ? "tutor" : "client",
-                    image: user.image,
-                };
-            },
-        }),
-    ],
-    callbacks: {
-        session: ({ session, token }) => {
-            console.log("Session Callback", { session, token });
-            return {
-                ...session,
-                user: {
-                    ...session.user,
-                    id: token.id,
-                    randomKey: token.randomKey,
-                },
-            };
-        },
-        jwt: ({ token, user }) => {
-            console.log("JWT Callback", { token, user });
-            if (user) {
-                const u = user as unknown as any;
-                token.id = u.id;
-                token.name = u.name;
-                token.email = u.email;
-                token.randomKey = u.randomKey as string;
-            }
-            return token;
-        },
-    },
+				return {
+					id: user.id + "",
+					email: user.email,
+					name: user.name,
+					randomKey: credentials?.typeOfTutor ? "tutor" : "client",
+					image: user.image,
+				};
+			},
+		}),
+	],
+	callbacks: {
+		session: ({ session, token }) => {
+			console.log("Session Callback", { session, token });
+			return {
+				...session,
+				user: {
+					...session.user,
+					id: token.id,
+					randomKey: token.randomKey,
+				},
+			};
+		},
+		jwt: ({ token, user }) => {
+			console.log("JWT Callback", { token, user });
+			if (user) {
+				const u = user as unknown as any;
+				token.id = u.id;
+				token.name = u.name;
+				token.email = u.email;
+				token.randomKey = u.randomKey as string;
+			}
+			return token;
+		},
+	},
 };
 
 const handler = NextAuth(authOptions);
-
-export const GET = handler;
-export const POST = handler;
+export { handler as GET, handler as POST };
--- tutor-connect/app/api/route.ts
+++ tutor-connect/app/api/route.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth/next";
 import { NextResponse } from "next/server";
-import { authOptions } from "./auth/[...nextauth]/route";
+import { authOptions } from "./auth/[...nextauth]/route.ts";
 
 export async function GET(request: Request) {
 	  const session = await getServerSession(authOptions);
