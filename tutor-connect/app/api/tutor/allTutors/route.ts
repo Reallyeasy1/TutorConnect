c097dca2b32d3c520a2ceceefc8e3f98f7f96e85
EthanLoww <156650597+EthanLoww@users.noreply.github.com>
2024-08-03 14:01:23 +0800

    Update route.ts

--- tutor-connect/app/api/tutor/allTutors/route.ts
+++ tutor-connect/app/api/tutor/allTutors/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
 	try {
 		const limit = 8;
