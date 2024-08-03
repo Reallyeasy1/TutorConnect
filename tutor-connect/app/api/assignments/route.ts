5e972060eceb58135badace1b802581fd1ac504d
Lo Yong Zhe <84175325+Reallyeasy1@users.noreply.github.com>
2024-08-03 13:09:33 +0800

    Update route.ts

--- tutor-connect/app/api/assignments/route.ts
+++ tutor-connect/app/api/assignments/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function POST() {
 	try {
 		const assignments = await prisma.assignment.findMany({
 			// where: { taken: false }, // Fetch only available assignments
